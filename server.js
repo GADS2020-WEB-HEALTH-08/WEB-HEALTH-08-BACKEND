186cef3bc4a9d8382e971028169155be09d94363
ChinonsoIg <ojononso@yahoo.com>
2020-11-03 19:45:23 +0100

    second commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
+const mysql = require('mysql');
 
 const initializePassport = require('./passport-config')
 initializePassport(
